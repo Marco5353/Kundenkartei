603304c6f5f7822a8ab08e295842a67cadac3c87
Marco5353 <56536300+Marco5353@users.noreply.github.com>
2019-12-23 10:43:45 +0100

    Add files via upload

---
+++ views/auswahl.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>Wasserwerke Login</title>
+	<link rel="stylesheet" type="text/css" href="login.css">
+</head>
+
+<body id="body">
+    <form method="post" action="zaehlerstand.php" >
+    <a href='processZae.php' class='button'><button>Eingabe Zählerstand</button></a>
+    </form>
+    <a href='register.php' class='button'><button>neuen Kunden anlegen</button></a>
+    <form method="post" action="abfrage.php" >
+    <a href='abfrage.php' class='button'><button>alle Zählerstände auflisten</button></a>
+    </form>
+    
+</body>
+	
+</html>
