4cfcaf5083de8a08ac1d0762975eda5741777808
Marco5353 <56536300+Marco5353@users.noreply.github.com>
2019-12-23 09:48:30 +0100

    Add files via upload

---
+++ abfrage.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Datenausgabe</title>
+	<link rel="stylesheet" type="text/css" href="login.css">
+</head>
+
+<body id="body">
+    <form action="processAbfrage.php" method="post">
+            Bitte geben sie nocheinmal ihre Zählernummer an:<input type="text" name="zaehlerNr">
+            <input type="submit" value="Bestätigen!"/>
+    </form>
+	
+</body>
+	
+</html>
